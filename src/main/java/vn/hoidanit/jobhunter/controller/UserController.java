1d8c3357cdefd80bb12ada1af107a67d4b711a20
vandang <vandang031001@gmail.com>
2025-04-02 23:27:10 +0700

    viet api CRUD co ban

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,10 +1,19 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+// import jakarta.validation.constraints.AssertFalse.List;
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class UserController {
@@ -14,17 +23,55 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public String createNewUser() {
+    // @GetMapping("/user/create")
+    @PostMapping("/user")
+    public User createNewUser(
+            @RequestBody User postManUser) {
 
-        User user = new User();
-        user.setEmail("vandang031001@gmail.com");
-        user.setName("Dang");
-        user.setPassword("123456");
+        // User user = new User();
+        // user.setEmail("vandang031001@gmail.com");
+        // user.setName("Dang");
+        // user.setPassword("123456");
 
-        this.userService.handleCreateUser(user);
+        User dUser = this.userService.handleCreateUser(postManUser);
 
-        return "create user";
+        return dUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "dUser";
+    }
+
+    // bai tap
+    // @GetMapping("/user/{id}")
+    // public User getUser(@PathVariable("id") User id) {
+    // User gUser = this.userService.handleGetUser(id);
+    // return gUser;
+    // }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id) {
+        User gUser = this.userService.handleFetchUserById(id);
+        return gUser;
+    }
+
+    // bai tap
+    // @GetMapping("/user")
+    // public List<User> getAllUser() {
+    // return new String();
+    // }
+
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+        return this.userService.fetchAllUser();
+    }
+
+    @PutMapping("user")
+    public User updateUser(@RequestBody User user) {
+        User pUser = this.userService.handleUpdateUser(user);
+        return pUser;
     }
 
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.User;
@@ -14,8 +17,42 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void handleCreateUser(User user) {
-        this.userRepository.save(user);
+    public User handleCreateUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public void handleDeleteUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+    // public User handleGetUser(long user) {
+    // return this.userRepository.save(user);
+    // }
+
+    public User handleFetchUserById(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    public List<User> fetchAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public User handleUpdateUser(User pUser) {
+        User currentUser = this.handleFetchUserById(pUser.getId());
+        if (currentUser != null) {
+            currentUser.setEmail(pUser.getEmail());
+            currentUser.setName(pUser.getName());
+            currentUser.setPassword(pUser.getPassword());
+
+            currentUser = this.userRepository.save(currentUser);
+
+        }
+
+        return currentUser;
     }
 
 }
